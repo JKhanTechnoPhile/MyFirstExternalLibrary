a8fa5837f5d8b3dbb474c4d64665cd661082ab90
Javeed Khan H <javeedkhanh1987@gmail.com>
2023-03-03 01:40:41 +0530

    Initial commit

---
+++ app/src/main/java/com/jkhan/myfirstexternallibrary/ExternlaLibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.jkhan.myfirstexternallibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//https://www.youtube.com/watch?v=ZnU21u1_fBE
+class ExternlaLibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_external_library)
+    }
+}
